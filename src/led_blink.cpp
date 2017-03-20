50e8dec2c817dbc09abb0d21f8dca5abfe815f9f
Vitor Greati <greati@ufrn.edu.br>
2017-03-20 09:43:47 -0300

    Include test for leds in real Beaglebone Black

---
+++ src/led_blink.cpp
@@ -0,0 +1,17 @@
+#include "Pin.h"
+#include <unistd.h>
+
+int main(void) {
+
+	Pin redled {"P9_14", GPIOSystem::Direction::OUT, GPIOSystem::Value::LOW};
+	Pin button {"P9_27", GPIOSystem::Direction::IN, GPIOSystem::Value::LOW};
+
+	while (true) {
+		if (button.getValue() == GPIOSystem::Value::HIGH)
+			redled.setValue(GPIOSystem::Value::HIGH);
+		else
+			redled.setValue(GPIOSystem::Value::LOW);
+	}
+
+	return 0;
+}
