de21ff12682f5dae80ceabc18ceb12789a80acad
Vinicius Campos <viniciuscampos120@gmail.com>
2017-03-20 11:53:43 -0300

    Change the code in check_cpu.cpp to be a function

--- src/check_cpu.cpp
+++ src/check_cpu.cpp
@@ -17,14 +17,7 @@ struct CpuInfo{
 	CpuInfo(std::string _cpu) : cpu{_cpu}, active{0}, total{0}{}
 };
 
-int main(int argn, char * argv[]){
-	
-	int sleep_time = 1000;
-
-	// Setting interval time to get cpu info
-	if(argn > 1){
-		sleep_time = atoi(argv[1]) * 1000;
-	}
+int check_cpu_usage(int sleep_time){
 
 	// Defining path file
 	std::string path = "/proc/stat";
@@ -32,52 +25,63 @@ int main(int argn, char * argv[]){
 	double usage;
 	std::ifstream cpu_status_file;
 	std::stringstream ss;
+
+	cpu_status_file.open(path, std::ifstream::in);
+	cpu = ""; usage = 0;
+
+	// Reading /proc/stat file 
+	getline(cpu_status_file, input);
+	ss << input;
+	ss >> cpu;
 	
-	std::vector< CpuInfo > prev_cpu_usage;
-	std::vector< CpuInfo > curr_cpu_usage;
+	// Storing prev use of each cpu
+	CpuInfo prev_cpu_usage = CpuInfo("Total");
+	for(int i = 0; i < 4; ++i){
+		ss >> usage;
+		if(i < 3) prev_cpu_usage.active += usage;
+		else prev_cpu_usage.total = prev_cpu_usage.active + usage;
+	}
+	ss.str(std::string());
+	ss.clear();
+	cpu_status_file.close();
 
-	bool first_time = true;
+	// Sleeping
+	std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+
+	cpu_status_file.open(path, std::ifstream::in);
+	cpu = ""; usage = 0;
+
+	// Reading /proc/stat file again
+	getline(cpu_status_file, input);
+	ss << input;
+	ss >> cpu;
+	
+	// Storing curr use of each cpu
+	CpuInfo curr_cpu_usage = CpuInfo("Total");
+	for(int i = 0; i < 4; ++i){
+		ss >> usage;
+		if(i < 3) curr_cpu_usage.active += usage;
+		else curr_cpu_usage.total = curr_cpu_usage.active + usage;
+	}
+	ss.str(std::string());
+	ss.clear();
+	cpu_status_file.close();
+
+	return 100 * (curr_cpu_usage.active - prev_cpu_usage.active) / (curr_cpu_usage.total - prev_cpu_usage.total);
+
+}
+
+int main(int argn, char * argv[]){
+	
+	int sleep_time = 1000;
+
+	// Setting interval time in milliseconds to get cpu info
+	if(argn > 1){
+		sleep_time = atoi(argv[1]);
+	}
+
 	while(true){
-		cpu_status_file.open(path, std::ifstream::in);
-		cpu = ""; usage = 0;
-		// Reading /proc/stat file 
-		while(getline(cpu_status_file, input)){
-			ss << input;
-			ss >> cpu;
-			// Checking if all cpus was read
-			if(cpu.compare("intr") == 0) {
-				ss.str(std::string());
-				ss.clear();
-				break;
-			}else{
-				// Storing current use of each cpu 		
-				curr_cpu_usage.push_back(CpuInfo(!cpu.compare("cpu") ? "Total" : cpu));
-				for(int i = 0; i < 4; ++i){
-					ss >> usage;
-					if(i < 3) curr_cpu_usage.back().active += usage;
-					else curr_cpu_usage.back().total = curr_cpu_usage.back().active + usage;
-				}
-				ss.str(std::string());
-				ss.clear();
-			}
-		}
-		cpu_status_file.close();
-
-		if(first_time){
-			prev_cpu_usage = curr_cpu_usage;
-			first_time = false;
-		}else{
-			// Printing cpu usage using prev_cpu_usage as reference (The same form of top and htop applications)
-			for(int i = 0; i < curr_cpu_usage.size(); ++i)
-				std::cout << curr_cpu_usage[i].cpu << ": " << std::setprecision(3) << 100 * (curr_cpu_usage[i].active - prev_cpu_usage[i].active) / (curr_cpu_usage[i].total - prev_cpu_usage[i].total) << "% | ";
-			std::cout << std::endl;
-			
-			// Updating prev_cpu_usage	
-			prev_cpu_usage = curr_cpu_usage;
-		}
-		curr_cpu_usage.clear();		
-		// Sleeping
-		std::this_thread::sleep_for (std::chrono::milliseconds(sleep_time));
+		std::cout << check_cpu_usage(sleep_time) << std::endl;
 	}
 
 	return 0;
